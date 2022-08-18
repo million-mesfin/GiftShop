6ff78d46de0217ceaf4e1d2dcfcc2438edde36e9
milla1114 <millionmesfind@gmail.com>
2022-08-18 21:07:26 +0300

    Added controllers

---
+++ GiftShop/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using GiftShop.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiftShop.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Categories.ToListAsync();
+            return View(data);
+        }
+    }
+}
---
+++ GiftShop/Controllers/ItemsController.cs
@@ -0,0 +1,24 @@
+﻿using GiftShop.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiftShop.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ItemsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Categories.ToListAsync();
+            return View();
+        }
+    }
+}
