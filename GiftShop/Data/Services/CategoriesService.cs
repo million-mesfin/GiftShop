c28a242cc15d30a5b9b25d508a9c4d09d458c91f
milla1114 <millionmesfind@gmail.com>
2022-08-19 11:58:01 +0300

    Added category sevices

---
+++ GiftShop/Data/Services/CategoriesService.cs
@@ -0,0 +1,44 @@
+﻿using GiftShop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiftShop.Data.Services
+{
+    public class CategoriesService : ICategoriesService
+    {
+        private readonly AppDbContext _context;
+        public CategoriesService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Category>> GetAll()
+        {
+            var result = await _context.Categories.ToListAsync();
+            return result;
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Update(int id, Category newCategory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GiftShop/Data/Services/ICategoriesService.cs
@@ -0,0 +1,17 @@
+﻿using GiftShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiftShop.Data.Services
+{
+    public interface ICategoriesService
+    {
+        Task<IEnumerable<Category>> GetAll();
+        Category GetById(int id);
+        void Add(Category category);
+        Category Update(int id, Category newCategory);
+        void Delete(int id);
+    }
+}
