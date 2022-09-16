882e716b87c526af01536d75645697be9dd91310
milla1114 <millionmesfind@gmail.com>
2022-09-16 23:42:36 +0300

    Implemented orders controller

---
+++ GiftShop/Controllers/OrdersController.cs
@@ -0,0 +1,58 @@
+﻿using GiftShop.Data.Cart;
+using GiftShop.Data.Services;
+using GiftShop.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiftShop.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IItemsService _itemsService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IItemsService itemsService, ShoppingCart shoppingCart)
+        {
+            _itemsService = itemsService;
+            _shoppingCart = shoppingCart;
+        }
+        public IActionResult ShoppingCart()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+
+        public async Task<IActionResult> AddItemToShoppingCart(int id)
+        {
+            var item = await _itemsService.GetItemById(id);
+
+            if (item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+        public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
+        {
+            var item = await _itemsService.GetItemById(id);
+
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+    }
+}
