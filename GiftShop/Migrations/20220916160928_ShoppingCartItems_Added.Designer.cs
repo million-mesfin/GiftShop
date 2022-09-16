ac065e3120834ce3c0e2ed9e09b26436dbe953fd
milla1114 <millionmesfind@gmail.com>
2022-09-16 23:42:03 +0300

    Order and shopping cart table migrations

--- GiftShop/Migrations/20220916160928_ShoppingCartItems_Added.Designer.cs
+++ GiftShop/Migrations/20220916160928_ShoppingCartItems_Added.Designer.cs
@@ -4,14 +4,16 @@ using GiftShop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GiftShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220916160928_ShoppingCartItems_Added")]
+    partial class ShoppingCartItems_Added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
