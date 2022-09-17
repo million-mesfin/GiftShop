8db254ffea2040047ea37335343036410bc7e2ae
milla1114 <millionmesfind@gmail.com>
2022-09-17 16:09:30 +0300

    Orders service implemented

---
+++ GiftShop/Data/Services/OrdersService.cs
@@ -0,0 +1,49 @@
+﻿using GiftShop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiftShop.Data.Services
+{
+    public class OrdersService : IOrdersService
+    {
+        private readonly AppDbContext _context;
+        public OrdersService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task<List<Order>> GetOrdersByUserId(string userId)
+        {
+            var orders = _context.Orders.Include(n => n.OrderItems).ThenInclude(n => n.Item).Where(n => n.UserId == userId).ToListAsync();
+            return orders;
+        }
+
+        public async Task StoreOrder(List<ShoppingCartItem> items, string userId, string userEmailAddress)
+        {
+            var order = new Order()
+            {
+                UserId = userId,
+                Email = userEmailAddress
+            };
+
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+
+            foreach (var item in items)
+            {
+                var orderItem = new OrderItem()
+                {
+                    Amount = item.Amount,
+                    ItemId = item.Item.Id,
+                    OrderId = order.Id,
+                    Price = item.Item.Price
+                };
+
+                await _context.OrderItems.AddAsync(orderItem);
+            }
+            await _context.SaveChangesAsync();
+        }
+    }
+}
